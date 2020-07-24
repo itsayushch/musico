01dd3697d45218b1d9aebe7e907e98004e4f6bb0
Ayush Chowdhury <ayush.chowdhury2004@gmail.com>
2020-07-24 13:59:33 +0530

    better embed

--- src/listeners/commandHandler/commandStarted.js
+++ src/listeners/commandHandler/commandStarted.js
@@ -1,4 +1,5 @@
 const { Listener } = require('discord-akairo');
+const { stripIndents } = require('common-tags');
 const Logger = require('../../util/logger');
 
 class CommandStartedListener extends Listener {
@@ -10,14 +11,21 @@ class CommandStartedListener extends Listener {
 		});
 	}
 
-	async exec(message, command, args) {
+	async exec(message, command) {
 		const level = message.guild ? `${message.guild.name}/${message.author.tag}` : `${message.author.tag}`;
 		Logger.log(`${command.id}`, { level });
 
 		const embed = this.client.util.embed()
 			.setColor('GREEN')
 			.setTitle(`Command used - ${command.id}`)
-			.setDescription(`\`\`\`js\n${command.id} - ${level}\`\`\``)
+			.setDescription(stripIndents`
+			\`\`\`js
+			COMMAND: ${command.id}
+
+			USER: ${message.author.tag} (${message.author.id})
+
+			GUILD: ${message.guild.name} (${message.guild.id})
+			\`\`\``)
 			.setTimestamp();
 		return this.client.channels.cache.get('736088895128600598').send({ embed });
 	}
