51c0fcc5e5af0a1015fafa6a0623b5dedbcf8a1a
Ayush Chowdhury <ayush.chowdhury2004@gmail.com>
2020-09-02 08:51:00 +0530

    fix instagram command

--- src/commands/util/instagram.js
+++ src/commands/util/instagram.js
@@ -1,65 +1,62 @@
-// const {Command} = require('discord-akairo')
-// const fetch = require("node-fetch");
+const { Command } = require('discord-akairo');
+const fetch = require('node-fetch');
 
-// module.exports = class IGCommand extends Command {
-//     constructor(){
-// 		super('instagram', {
-// 			aliases: ['instagram', 'insta' , 'ig'],
-// 			category: 'utility',
-// 			clientPermissions: ['SEND_MESSAGES'],
-// 			args: [
-// 				{
-// 					id: 'name',
-// 					type: 'string',
-// 					match: 'content',
-// 					prompt:{
-// 						start:'Enter your username'
-// 					}
-// 				},
-// 			],
-// 			description: {
-// 				content: 'Searches your instagram profile',
-// 				usage: '[username]',
-// 				examples: ['ayushkr_04']
-// 			}
-//         });
-//     }
-//     async exec(message, {name}) {
-//         async function getUser(q){
-//         const url = `https://instagram.com/${q}/?__a=1`;
+module.exports = class IGCommand extends Command {
+	constructor() {
+		super('instagram', {
+			aliases: ['instagram', 'insta', 'ig'],
+			category: 'utility',
+			clientPermissions: ['SEND_MESSAGES'],
+			args: [
+				{
+					id: 'name',
+					type: 'string',
+					match: 'content',
+					prompt: {
+						start: 'Enter your username'
+					}
+				}
+			],
+			description: {
+				content: 'Searches your instagram profile',
+				usage: '[username]',
+				examples: ['ayushkr_04']
+			}
+		});
+	}
 
-//         let res;
+	async exec(message, { name }) {
+		const res = await this.getUser(name);
+		const notFound = Object.entries(res);
+		if (!notFound.length) {
+			return message.util.send({
+				embed: {
+					color: 'RED',
+					description: `No users found! with the name: **${name}**`
+				}
+			});
+		}
+		const account = res.graphql.user;
+		const embed = this.client.util.embed()
+			.setColor(0x5e17eb)
+			.setTitle(account.full_name)
+			.setURL(`https://instagram.com/${name}`)
+			.setThumbnail(account.profile_pic_url_hd)
+			.setDescription(`>>> ${account.biography}`)
+			.addField('Username: ', `${account.username}`)
+			.addField('Full name:', `${account.full_name}`)
+			.addField('Posts:', `${account.edge_owner_to_timeline_media.count}`)
+			.addField('Followers:', `${account.edge_followed_by.count}`)
+			.addField('Following:', `${account.edge_follow.count}`)
+			.addField('Account type:', account.is_private ? 'Private 🔐' : 'Public 🔓');
+		return message.util.send(embed);
+	}
 
-//         try {
-//             res = await fetch(url).then(url => url.json());
-//             return res;
-//         } catch (e) {
-// 			message.reply(`User not found`);
-// 			throw e;
-//         }
-//     }
-//         const res = await getUser(name)
-//         const account = res.graphql.user;
-//         const embed = this.client.util.embed()
-//             .setColor(0x5e17eb)
-//             .setTitle(account.full_name)
-//             .setURL(`https://instagram.com/${name}`)
-//             .setThumbnail(account.profile_pic_url_hd)
-//             .setDescription("Profile information")
-//             .addField("**- Username:** ",`${account.username}`)
-//             .addField("**- Full name:**" ,`${account.full_name}`)
-//             if(account.biography.length) embed.addField("**- Biography**",` >>> ${account.biography}`)
-//             .addField('**- Posts:**' ,`${account.edge_owner_to_timeline_media.count}`)
-//             .addField('**- Followers:**', `${account.edge_followed_by.count}`)
-//             .addField('**- Following:**', `${account.edge_follow.count}`)
-//             if(account.is_private) embed.addField('**- Account type:**', "Private 🔐")
-//             if(!account.is_private) embed.addField('**- Account type:**', "Public 🔓")
-//             .setFooter('Powered by instagram API')
-//             .setTimestamp();
+	async getUser(name) {
+		const url = `https://instagram.com/${name}/?__a=1`;
 
-//         message.channel.send(embed);
-
-
-//     }
-
-// }
+		const res = await fetch(url);
+		const data = await res.json();
+		return data;
+	}
+};
