3b2aac73fa5afca044169119db77fcea71641eb8
Ayush Chowdhury <ayush.chowdhury2004@gmail.com>
2020-10-14 18:42:01 +0530

    ignore bots

--- src/listeners/client/message.js
+++ src/listeners/client/message.js
@@ -10,7 +10,7 @@ class Message extends Listener {
 	}
 
 	async exec(message) {
-        if (!message.guild || message.guild.id !== '694554848758202451') return;
+        if (!message.guild || message.guild.id !== '694554848758202451' || message.author.bot) return;
         
         const score = await this.client.settings.get(message.guild.id, message.author.id, {
             level: 0,
