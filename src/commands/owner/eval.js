8f35b4b10c023112087d0d09c1e521d70dc2b08b
Ayush Chowdhury <ayush.chowdhury2004@gmail.com>
2020-11-28 18:34:03 +0530

    eval command

--- src/commands/owner/eval.js
+++ src/commands/owner/eval.js
@@ -1,112 +1,90 @@
-const { Command } = require('discord-akairo');
+const { Command, Flag, Argument } = require('discord-akairo');
+const { Util } = require('discord.js');
 const util = require('util');
 
 class EvalCommand extends Command {
 	constructor() {
 		super('eval', {
 			aliases: ['eval', 'e'],
-			ownerOnly: true,
-			quoted: false,
 			category: 'owner',
-			clientPermissions: ['SEND_MESSAGES'],
-			args: [
-				{
-					id: 'code',
-					match: 'content',
-					prompt: {
-						start: 'What would you like to evaluate ?',
-						retry: 'Try again.'
-					}
-				}
-
-			]
+			optionFlags: ['--depth', '-d'],
+			ownerOnly: true,
+			description: {
+				content: 'You can\'t use this anyway, so why explain?',
+				usage: '<code>'
+			}
 		});
+
+		this.eval = null;
 	}
 
-	async exec(message, { code }) {
-		if (!code) {
-			return message.util.send('No code provided!');
-		}
-
-		const evaled = {};
-		const logs = [];
-		const token = this.client.token.split('').join('[^]{0,2}');
-		const rev = this.client.token.split('').reverse().join('[^]{0,2}');
-		const tokenRegex = new RegExp(`${token}|${rev}`, 'g');
-		const cb = '```';
-		const print = (...a) => { // eslint-disable-line no-unused-vars
-			const cleaned = a.map(obj => {
-				if (typeof o !== 'string') obj = util.inspect(obj, { depth: 1 });
-				return obj.replace(tokenRegex, '[TERE TOH L LAG GAYE]');
-			});
-
-			if (!evaled.output) {
-				logs.push(...cleaned);
-				return;
-			}
-
-			evaled.output += evaled.output.endsWith('\n') ? cleaned.join(' ') : `\n${cleaned.join(' ')}`;
-			const title = evaled.errored ? '☠\u2000**Error**' : '📤\u2000**Output**';
-			if (evaled.output.length + code.length > 1900) {
-				evaled.output = 'Output too long.';
-			}
-
-			evaled.message.edit([
-				`${title}${cb}js`,
-				evaled.output,
-				cb
-			]);
+	*args() {
+		const depth = yield {
+			match: 'option',
+			type: Argument.range('integer', 0, 3, true),
+			flag: ['--depth', '-d'],
+			default: 0
 		};
+
+		const code = yield {
+			match: 'rest',
+			type: (msg, code) => {
+				if (!code) return Flag.cancel();
+				return code;
+			}
+		};
+
+		return { code, depth };
+	}
+
+	async exec(message, { code, depth }) {
 		let hrDiff;
 		try {
 			const hrStart = process.hrtime();
-			// eslint-disable-next-line no-eval
-			let output = eval(code);
+			this.eval = eval(code); // eslint-disable-line
 			hrDiff = process.hrtime(hrStart);
-			// eslint-disable-next-line eqeqeq
-			if (output != null && typeof output.then === 'function') {
-				output = await output;
-			}
-
-			if (typeof output !== 'string') {
-				output = util.inspect(output, { depth: 0 });
-			}
-
-			output = `${logs.join('\n')}\n${logs.length && output === 'undefined' ? '' : output}`;
-			output = output.replace(tokenRegex, '[TERE TOH L LAG GAYE]');
-			if (output.length + code.length > 1900) {
-				output = 'Output too long.';
-			}
-			this.hrStart = process.hrtime();
-			const sent = await message.util.send(
-				[
-					`**Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms**`,
-					`📤\u2000**Output**${cb}js`,
-					output,
-					cb
-				]
-			);
-
-			evaled.message = sent;
-			evaled.errored = false;
-			evaled.output = output;
-			return sent;
-		} catch (err) {
-			let error = err;
-			error = error.toString();
-			error = `${logs.join('\n')}\n${logs.length && error === 'undefined' ? '' : error}`;
-			error = error.replace(tokenRegex, '[TERE TOH L LAG GAYE]');
-			const sent = await message.util.send([
-				`☠\u2000**Error**${cb}js`,
-				error,
-				cb
+		} catch (error) {
+			return message.util.send([
+				'**☠\u2000**Error****',
+				`\`\`\`js\n${error}\n\`\`\``
 			]);
-
-			evaled.message = sent;
-			evaled.errored = true;
-			evaled.output = error;
-			return sent;
 		}
+
+		this.hrStart = process.hrtime();
+		const result = this.result(await this.eval, hrDiff, code, depth);
+		if (Array.isArray(result)) return result.map(async res => message.util.send(res));
+		return message.util.send(result);
+	}
+
+	result(result, hrDiff, input, depth) {
+		const inspected = util.inspect(result, { depth }).replace(new RegExp('!!NL!!', 'g'), '\n').replace(this.replaceToken, '--🙄--');
+		const split = inspected.split('\n');
+		const last = inspected.length - 1;
+		const prependPart = inspected[0] !== '{' && inspected[0] !== '[' && inspected[0] !== '\'' ? split[0] : inspected[0];
+		const appendPart = inspected[last] !== '}' && inspected[last] !== ']' && inspected[last] !== '\'' ? split[split.length - 1] : inspected[last];
+		const prepend = `\`\`\`js\n${prependPart}\n`;
+		const append = `\n${appendPart}\n\`\`\``;
+		if (input) {
+			return Util.splitMessage([
+				`*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms*`,
+				'📤\u2000**Output**',
+				`\`\`\`js\n${inspected}\`\`\``
+			], {
+				maxLength: 1900, prepend, append
+			});
+		}
+		return Util.splitMessage(`*Callback executed after ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms* \`\`\`js\n${inspected}\`\`\``, {
+			maxLength: 1900, prepend, append
+		});
+	}
+
+	get replaceToken() {
+		if (!this._replaceToken) {
+			const token = this.client.token.split('').join('[^]{0,2}');
+			const revToken = this.client.token.split('').reverse().join('[^]{0,2}');
+			Object.defineProperty(this, '_replaceToken', { value: new RegExp(`${token}|${revToken}`, 'g') });
+		}
+		return this._replaceToken;
 	}
 }
 
