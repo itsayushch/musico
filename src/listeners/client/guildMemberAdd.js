e3533c6a140cd65c26fdbd6c63fb99b2398619aa
itsayushch <ayush.chowdhury2004@gmail.com>
2021-02-09 13:57:45 +0530

    automatically assign roles to verified bots

--- src/listeners/client/guildMemberAdd.js
+++ src/listeners/client/guildMemberAdd.js
@@ -1,5 +1,6 @@
 const { Listener } = require('discord-akairo');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, GuildMember } = require('discord.js');
+
 class GuildMemberAddListener extends Listener {
 	constructor() {
 		super('guildMemberAddMemberLog', {
@@ -10,6 +11,7 @@ class GuildMemberAddListener extends Listener {
 	}
 
 	async exec(member) {
+		if (member.guild.id === '694554848758202451') await this.handleVerifiedBots(member);
 		const memberlog = this.client.settings.get(member.guild.id, 'member-log', undefined);
 		if (memberlog) {
 			const embed = new MessageEmbed()
@@ -21,6 +23,18 @@ class GuildMemberAddListener extends Listener {
 				.setTimestamp();
 
 			return this.client.channels.cache.get(memberlog).send(embed);
+		}
+	}
+
+	/**
+	 *
+	 * @param {GuildMember} member - The guild member
+	 */
+	async handleVerifiedBots(member) {
+		const { approved } = await this.client.mongo.db('musico').collection('bots').updateOne({ clientID: member.id });
+
+		if (approved) {
+			return member.roles.add('808307235590766602');
 		}
 	}
 }
