a19b8a894b70344d15def1a1937d6c2ce213e26f
YasH⚡ <74340276+YashSaini99@users.noreply.github.com>
2021-06-09 12:03:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ require('dotenv').config();
 const BotClient = require('./src/struct/BotClient');
 const client = new BotClient();
 
+//=========Message me daal lio =====//
+// if(message.channel.id === 'AI channel id') {
+//         fetch(`https://api.monkedev.com/fun/chat?msg=${message.content}&uid=${message.author.id}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             message.lineReplyNoMention(data.response)
+//         })
+//         .catch(() => {
+//             message.lineReplyNoMention("Couldn't fetch Response!!");
+//         })
+//     }
 client.start(process.env.TOKEN);
